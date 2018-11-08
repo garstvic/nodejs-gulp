aca24ec591b6220b0077b5ea6b84b5f7ceec3ef2
Victor Garstukov <garstvic@gmail.com>
2018-11-08 14:54:46 +0000

    Minify Scripts file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
 gulp.task('concatScripts', function() {
     gulp.src([
@@ -10,6 +12,15 @@ gulp.task('concatScripts', function() {
         'js/main.js'])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('js'));
+});
+
+gulp.task('minifyScripts', function() {
+    gulp.src(
+        'js/app.js' 
+        )
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('js')); 
 });
 
 gulp.task('default', ['concatScripts'], function() {
